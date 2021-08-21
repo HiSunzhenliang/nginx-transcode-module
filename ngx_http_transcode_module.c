ee1fa00e3771f039a6c888b4888ff469caac5ed3
sunzhenliang <hisunzhenliang@outlook.com>
2021-08-21 13:36:43 +0800

    Response hello

--- ngx_http_transcode_module.c
+++ ngx_http_transcode_module.c
@@ -6,7 +6,60 @@
 #include "ngx_http_transcode_module.h"
 
 static ngx_int_t ngx_http_transcode_handler(ngx_http_request_t *r) {
-    return 1;
+    ngx_int_t err;
+    status_code code;
+    ngx_log_t *log;
+    ngx_chain_t out;
+    ngx_buf_t *buff;
+    ngx_http_transcode_loc_conf_t *conf;
+    ngx_str_t root = ngx_null_string;
+    ngx_str_t output_format = ngx_null_string;
+    ngx_str_t output = ngx_null_string;
+    ngx_str_t path = ngx_null_string;
+
+    conf = ngx_http_get_module_loc_conf(r, ngx_http_transcode_module);
+    ngx_http_complex_value(r, conf->root, &root);
+    ngx_http_complex_value(r, conf->output_format, &output_format);
+
+    err = ngx_http_discard_request_body(r);
+    if (err != NGX_OK) {
+        return err;
+    }
+
+    log = r->connection->log;
+    path = generate_path(r->pool, root, r->uri);
+    code = transcode(&output, r->pool, log, path, output_format);
+
+    if (code) {
+        code = NGX_HTTP_TRANSCODE_MODULE_FOUND;
+    }
+
+    u_char* tmp = ngx_palloc(r->pool,100);
+    ngx_sprintf(tmp,"hello ngx");
+    output.data = tmp;
+    output.len = ngx_strlen(tmp);
+    /* todo */
+
+    buff = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    if (buff == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+    buff->pos = output.data;
+    buff->last = output.data + (output.len * sizeof(unsigned char));
+    buff->memory = 1;
+    buff->last_buf = 1;
+
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = output.len;
+
+    out.buf = buff;
+    out.next = NULL;
+    err = ngx_http_send_header(r);
+    if (err == NGX_ERROR || err > NGX_OK || r->header_only) {
+        return err;
+    }
+
+    return ngx_http_output_filter(r, &out);
 }
 
 static char *ngx_http_transcode(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
@@ -54,4 +107,15 @@ static char *ngx_http_transcode_merge_loc_conf(ngx_conf_t *cf, void *parent,
     }
 
     return NGX_CONF_OK;
+}
+
+static ngx_str_t generate_path(ngx_pool_t *pool, ngx_str_t rootpath, ngx_str_t uri) {
+    /* todo */
+    ngx_str_t tmp = ngx_null_string;
+    return tmp;
+}
+
+static ngx_int_t transcode(ngx_str_t *output, ngx_pool_t *pool, ngx_log_t *log, ngx_str_t source, ngx_str_t fmt) {
+    /* todo */
+    return 0;
 }
