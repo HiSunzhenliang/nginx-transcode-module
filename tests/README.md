1b772fdb6639142fb1d8f35cbabe385299bcc135
sunzhenliang <hisunzhenliang@outlook.com>
2021-09-02 18:05:31 +0800

    Upgrade tests script
    Add nginx.conf for tests, and more formats needs tests.

---
+++ tests/README.md
@@ -0,0 +1,9 @@
+# nginx-transcode-module test
+## how
+1. launch nginx with test-nginx.conf
+2. make test
+
+## Add test
+
+For now, it only supports sox's supported audio formats.
+
--- tests/test.sh
+++ tests/test.sh
@@ -1,45 +1,80 @@
 #!/bin/bash
-# ./test.sh host listfile outputdir
 
-if [ $# -ne 3 ]; then
-    echo "Params error"
-    echo "Example: ./test.sh 127.0.0.1:8000 test_audios.txt out"
-    exit
-else
-    echo "Go tests:"
+sourcetypes=("pcm" "alaw" "ulaw" "amrnb" "amrwb" "mpeg")
+outtypes=(wav,mp3)
+
+sourcedir=/tmp/tmp_transcode_root
+outdir=outdir
+tmplist=tmp_list.txt
+sourcefile=addf8-Alaw-GW.wav
+host=127.0.0.1:8080
+
+echo "Go tests:"
+
+rm -rf $sourcedir $outdir $tmp_list $sourcefile
+
+# make test audios
+echo "1. make test audios"
+wget -q http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/Samples/Goldwave/addf8-Alaw-GW.wav
+if [ $? -ne 0 ]; then
+    echo "Get source file failed"
+    exit -1
 fi
 
-host=$1
-listfile=$2
-outdir=$3
+mkdir -p $sourcedir
+if [ $? -ne 0 ]; then
+    echo "Mkdir source file dir failed"
+    exit -1
+fi
 
-if [ -d "$outdir" ]; then
-    echo "$outdir exists, exit"
-    exit
-else
-    mkdir $outdir
-    if [ $? -ne 0 ]; then
-        echo "mkdir $outdir failed, exit"
-        exit
+for i in ${sourcetypes[@]}; do
+    if [ "$i" == "pcm" ]; then
+        sox $sourcefile -e signed-integer $sourcedir/$i.wav
+    elif [ "$i" == "alaw" ]; then
+        sox $sourcefile -e a-law $sourcedir/$i.wav
+    elif [ "$i" == "ulaw" ]; then
+        sox $sourcefile -e u-law $sourcedir/$i.wav
+    elif [ "$i" == "amrnb" ]; then
+        sox $sourcefile -t amr-nb $sourcedir/$i.amr-nb
+    elif [ "$i" == "amrwb" ]; then
+        sox $sourcefile -r 16k -t amr-wb $sourcedir/$i.amr-wb
+    elif [ "$i" == "mpeg" ]; then
+        sox $sourcefile $sourcedir/$i.mp3
     fi
+done
+
+# make test list file
+# needs combinations of formats
+echo "2. make test list file"
+ls $sourcedir > $tmplist
+
+
+# check output dir
+rm -rf $outdir
+mkdir $outdir
+if [ $? -ne 0 ]; then
+    echo "mkdir $outdir failed, exit"
+    exit -1
 fi
 
-for audio in $(cat $listfile); do
-    cd $outdir
+# run test
+for audio in $(cat $tmplist); do
     wget -q http://$host/$audio
     if [ $? -ne 0 ]; then
         echo "failed: $audio"
-        cd - > /dev/null
+        rm $audio
         continue
     fi
 
-    cd - > /dev/null
-    err=`soxi $outdir/$audio 2>&1`
+    err=$(soxi $audio 2>&1)
     if [ $? -ne 0 ]; then
         echo "failed: $audio ($err)"
+        rm $audio
         continue
     fi
     echo "succss: $audio"
+    rm $audio
 done
 
+rm -rf $sourcedir $outdir $tmplist $sourcefile
 echo "Tests done"
