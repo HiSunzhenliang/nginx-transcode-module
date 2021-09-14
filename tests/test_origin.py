c006ba7ecb06292630fa87a4d8cea7e4dee9eebc
sunzhenliang <hisunzhenliang@outlook.com>
2021-09-14 19:25:55 +0800

    Download origins
    Add origin url in origin.list use make origin to download. Make sure do
    make clean to remove work dir first.

--- tests/test_origin.py
+++ tests/test_origin.py
@@ -1,51 +1,15 @@
 #!/usr/bin/env python3
-
-# @file transcode_test.py
+# @file test_origin.py
 # @author SunZhenliang
 
 import os
-import subprocess
 import urllib.request
 import json
 
-# SOURCE = [(CODING_FORMAT, CONTAINER_FORMAT)]
-# TARGET = [(CODING_FORMAT, CONTAINER_FORMAT)]
-
 WORK_DIR = "work_dir"
 ORIGIN_DIR = WORK_DIR + "/" + "origin"
-TARGET_DIR = WORK_DIR + "/" + "target"
 
 ORIGIN_LIST = "origin.list"
-
-SOURCE = [
-    ("signed-integer","wav"),
-    ("a-law", "wav"),
-    ("u-law", "wav"),
-    ("amrnb", "amr"),
-    ("amrwb", "awb"),
-    ("MPEG", "mp3")
-]
-
-TARGET = [
-    ("signed-integer","wav"),
-    ("MPEG", "mp3")
-]
-
-
-def listdir(path, list_name):
-    for file in os.listdir(path):
-        file_path = os.path.join(path, file)
-        if os.path.isdir(file_path):
-            listdir(file_path, list_name)
-        else:
-            list_name.append(file_path)
-
-
-def callcmd(cmd):
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print(f'ERROR cmd: {cmd}')
-
 
 def download_origin():
     os.mkdir(ORIGIN_DIR)
@@ -65,9 +29,12 @@ def download_origin():
 def main():
     os.mkdir(WORK_DIR)
     origin_list = download_origin()
-    print(f"Inputs:")
+    print(f"\nOrigins:")
     for i in origin_list:
         print(i)
+
+    path = os.getcwd() + "/" + ORIGIN_DIR
+    print(f"\nAdd this in nginx.conf: \ntranscode_root \"{path}\";\n")
 
 
 if __name__ == '__main__':
